a0f27678c07bab9d6b620bd1d9090d44132dcad5
tatsuhiko-e <70308705+tatsuhiko-e@users.noreply.github.com>
2022-12-17 23:51:05 +0900

    Pagelayout update

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -1,50 +1,35 @@
 import React, { Fragment, useState } from 'react';
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
 import { menuItems } from '../routes/route';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { IconButton } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const { Header, Sider, Content, Footer } = Layout;
 
+const HeadIcon = styled.div`
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+`
+
+const UserName = styled.div`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+  margin-right: 30px;
+`
+
+
 const PageLayout = ({ children }: any) => {
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
-  const onCollapse = (collapsed: any) => {
-    console.log('onCollapse', collapsed);
-    // this.props.dispatch({
-    //   type: 'global/changeLayoutCollapsed',
-    //   payload: collapsed,
-    // });
-  }
-
-  const HeadIcon = styled.div`
-    height: 32px;
-    background: rgba(255, 255, 255, 0.2);
-    margin: 16px;
-  `
-
   return (
     <Fragment>
       <Layout>
         <Sider
           breakpoint="lg"
           collapsedWidth="0"
-          onBreakpoint={broken => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
         >
           <HeadIcon />
           <Menu
@@ -64,7 +49,13 @@ const PageLayout = ({ children }: any) => {
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Header className="header" style={{ display: "flex", justifyContent: "end" }}>
+            <div style={{ display: "flex" }}>
+              <UserName>江口正樹</UserName>
+              <IconButton onClick={()=> console.log("aaaa")}>
+                <LogoutIcon sx={{ color: "#fff" }} style={{ fontSize: "35px" }} />
+              </IconButton>
+            </div>
           </Header>
           <Content style={{ margin: '24px 16px 0', height: "100vh" }}>
             {children}
